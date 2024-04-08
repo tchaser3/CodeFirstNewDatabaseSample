b86cc2d1ac4c668a187c24966453411003c69a46
Terry Holmes <tchaser3@outlook.com>
2024-04-08 06:44:53 -0400

    Add project files.

---
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Title:           Testing the Entity Framework
+ * Date:            4-30-18
+ * Author:          Terry Holmes
+ * 
+ * Description:     This is a test of the entity framework*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+        }
+        
+    }
+}
